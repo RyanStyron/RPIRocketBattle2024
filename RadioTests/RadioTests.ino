a72d9c0a8fc5e939feb11289c69676054e017332
Ryan Styron <ryanstyron1220@gmail.com>
2024-01-31 20:18:08 -0500

    Update RadioTests.ino with more compact logic; remove unnecessary comments.

--- RadioTests/RadioTests.ino
+++ RadioTests/RadioTests.ino
@@ -1,72 +1,51 @@
 /*
-  Software serial multiple serial test
+  Software serial test intended for Arduino UNO R3.
 
- Receives from the two software serial ports,
- sends to the hardware serial port.
+  Receives from the two software serial ports, sends to the hardware serial port.
 
- In order to listen on a software port, you call port.listen().
- When using two software serial ports, you have to switch ports
- by listen()ing on each one in turn. Pick a logical time to switch
- ports, like the end of an expected transmission, or when the
- buffer is empty. This example switches ports when there is nothing
- more to read from a port
+  In order to listen on a software port, you call port.listen().
+  When using two software serial ports, you have to switch ports
+  by listen()ing on each one in turn. Pick a logical time to switch
+  ports, like the end of an expected transmission, or when the
+  buffer is empty. This example switches ports when there is nothing
+  more to read from a port
 
- The circuit:
- Two devices which communicate serially are needed.
- * First serial device's TX attached to digital pin 10(RX), RX to pin 11(TX)
- * Second serial device's TX attached to digital pin 8(RX), RX to pin 9(TX)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created 18 Apr. 2011
- modified 19 March 2016
- by Tom Igoe
- based on Mikal Hart's twoPortRXExample
-
- This example code is in the public domain.
-
+  The circuit:
+  Two devices which communicate serially are needed.
+  * First serial device's TX attached to digital pin 11, RX attached to digital pin 10.
  */
-
 #include <SoftwareSerial.h>
-// software serial #1: RX = digital pin 10, TX = digital pin 11
-SoftwareSerial portOne(11, 10);
 
-// software serial #2: RX = digital pin 8, TX = digital pin 9
-// on the Mega, use other pins instead, since 8 and 9 don't work on the Mega
+// Receiver pin.
+const byte rxPin = 10;
+// Transmitter pin.
+const byte txPin = 11;
+
+// XBee Radio software serial connection. 
+SoftwareSerial xbee_radio(txPin, rxPin);
 
 void setup() {
-  // Open serial communications and wait for port to open:
+  // Open serial communications and wait for port to open.
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
 
+    while (!Serial)
+    // Wait for serial port to connect -- necessary for native USB port only.
+    ;
+  // Output to the serial monitor that the program has initialized.
   Serial.println("Initialized");
-  // Start each software serial port
-  portOne.begin(9600);
+  // Start each software serial port with the suggested baud rate.
+  xbee_radio.begin(9600);
 }
 
 void loop() {
-  // By default, the last initialized port is listening.
-  // when you want to listen on a port, explicitly select it:
-  portOne.listen();
-  // while there is data coming in, read it
-  // and send to the hardware serial port:
-  while (portOne.available() > 0) {
-    char inByte = portOne.read();
-    //Serial.println("Data from port one:");
-    Serial.write(inByte);
-    //Serial.println();
-  }
-  portOne.print("Ayo");
-  delay(100);
+  // By default, the last initialized port is listening. Otherwise, explicitly select it.
+  xbee_radio.listen();
 
-  // blank line to separate data from the two ports:
+  // While there is data being read, send to the serial port.
+  while (xbee_radio.available() > 0)
+    Serial.println(xbee_radio.read());
+  // Send message to the XBee.
+  xbee_radio.println("Test");
+  // Delay the loop as to not cause a continual spam and to allow other byte receipts.
+  delay(100);
 }
