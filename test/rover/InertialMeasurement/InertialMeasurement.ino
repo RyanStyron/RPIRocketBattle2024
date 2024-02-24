1259e76001adb4faf6e9b5da8580010a8df4d787
RyanStyron <ryanstyron1220@gmail.com>
2024-02-24 12:46:32 -0500

    Add inertial measurement file description.

--- test/rover/InertialMeasurement/InertialMeasurement.ino
+++ test/rover/InertialMeasurement/InertialMeasurement.ino
@@ -1,3 +1,10 @@
+/**
+ * Provides rudimentary testing of the BerryIMU sensor, outputting the acceleration and angular velocity
+ * values to the serial monitor. 
+ * 
+ * In this test, readings are taken every 2.5 seconds, and are ouputted to the serial monitor.
+ * In the production version, the readings will be sent through the XBee radio to the ground station.
+*/
 #include <BerryIMU.h>
 #include <Wire.h>
 
@@ -18,13 +25,19 @@ void loop() {
 
     // Outputting acceleration values to the serial monitor.
     Serial.print("Acceleration (x, y, z): ");
-    Serial.print(accelX); Serial.print(", ");
-    Serial.print(accelY); Serial.print(", ");
+    Serial.print(accelX); 
+    Serial.print(", ");
+    Serial.print(accelY); 
+    Serial.print(", ");
     Serial.println(accelZ);
 
     // Outputting gyroscope values (degrees per second) to the serial monitor.
     Serial.print("Angular Velocity (x, y, z): ");
-    Serial.print(gyroX); Serial.print(", ");
-    Serial.print(gyroY); Serial.print(", ");
+    Serial.print(gyroX); 
+    Serial.print(", ");
+    Serial.print(gyroY);
+    Serial.print(", ");
     Serial.println(gyroZ);
+
+    delay(250);
 }
