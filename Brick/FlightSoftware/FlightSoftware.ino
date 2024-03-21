6e094df3b31962eb006c4528251a33d75c89cc14
RyanStyron <ryanstyron1220@gmail.com>
2024-03-20 20:04:25 -0400

    Update flight software.

--- Brick/FlightSoftware/FlightSoftware.ino
+++ Brick/FlightSoftware/FlightSoftware.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
-//#include <BerryIMU.h>
+// #include <BerryIMU.h>
 #include <Wire.h>
 #include "Arducam_Mega.h"
 
@@ -21,6 +21,7 @@ unsigned int counter = 0;
 uint8_t imageBuff[BUFFER_SIZE] = {0};
 
 Arducam_Mega camera(chipSelectPin);
+
 int pictureDelay = 100; // Time between deployment and image capture
 
 //BerryIMU berry;
@@ -40,101 +41,101 @@ bool deployed = false; // Keeps track of if the brick has been deployed
 byte flightMode = 0; // 0 is idle; 1 is pad + flight + landing; 2 is ejection
 
 void setup() {
-  Serial.begin(9600); // Open serial communications and wait for port to open.
+    Serial.begin(9600); // Open serial communications and wait for port to open.
 
-  while (!Serial) ; // Wait for serial port to connect -- necessary for native USB port only.
+    while (!Serial); // Wait for serial port to connect -- necessary for native USB port only.
+    xbee_radio.begin(9600); // Start each software serial port with the suggested baud rate.
+    camera.begin();
+    retentionServo.attach(9);  // attaches the servo on pin 9 to the servo object
+    // berry.begin();
+    Wire.begin();
+    Serial.write("~~Arduino Powered~~"); // Output to the serial monitor that the program has initialized.
 
-  Serial.println("Initialized"); // Output to the serial monitor that the program has initialized.
-
-  xbee_radio.begin(115200); // Start each software serial port with the suggested baud rate.
-
-  camera.begin();
-
-  retentionServo.attach(9);  // attaches the servo on pin 9 to the servo object
-  
-  //berry.begin();
-  Wire.begin();
-
-  pinMode(voltageSensor, INPUT);
+    pinMode(voltageSensor, INPUT);
 }
 
 void loop() {
-  xbee_radio.listen(); // By default, the last initialized port is listening. Otherwise, explicitly select it.
-  
-  while (xbee_radio.available() > 0)
-    flightMode = xbee_radio.read();
+    xbee_radio.listen(); // By default, the last initialized port is listening. Otherwise, explicitly select it.
 
-  if(flightMode == 0 || deployed){
-    retentionServo.write(releasedAngle); // keeps servo in deployed position
-  } else if(flightMode == 1){
-    retentionServo.write(retainedAngle); // locks servo
+    while (xbee_radio.available() > 0)
+        flightMode = xbee_radio.read();
+    Serial.println(flightMode);
 
-    //berry.readAccelerometer(&accelX, &accelY, &accelZ); // read accelerion data
-    //berry.readGyroscope(&gyroX, &gyroY, &gyroZ); // read rotational data
-    batteryVoltage = analogRead(voltageSensor)*(11.0/430.0);
 
-    xbee_radio.println(0xc8); // indicate start of normal data packet
-    xbee_radio.println(0x01); // nominal status
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(accelX); // send accelerometer reading
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(accelY); // send accelerometer reading
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(accelZ); // send accelerometer reading
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(gyroX); // send gyro reading
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(gyroY); // send gyro reading
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(gyroZ); // send gyro reading
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(temperature); // send temperature reading
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(batteryVoltage); // send voltage reading
-    xbee_radio.println(0xc9); // mark separation between values
-    xbee_radio.println(altitude); // send altitude reading
-    xbee_radio.println(0xca); // indicate end of normal data packet
-  } else if(flightMode == 2){
-    retentionServo.write(releasedAngle); // keeps servo in deployed position
+    if (flightMode == 0 || deployed) {
+        retentionServo.write(releasedAngle); // keeps servo in deployed position
+    } else if(flightMode == 1){
+        retentionServo.write(retainedAngle); // locks servo
 
-    delay(pictureDelay);
+        // berry.readAltitude(&altitude); // read altitude data
+        // berry.readTemperature(&temperature); // read temperature data
+        // berry.readAccelerometer(&accelX, &accelY, &accelZ); // read accelerion data
+        // berry.readGyroscope(&gyroX, &gyroY, &gyroZ); // read rotational data
+        batteryVoltage = analogRead(voltageSensor)*(11.0/430.0);
 
-      // Capture image in JPG format using an image resolution of one of the following: QQVGA, QVGA, VGA, HD, FHD.
-    camera.takePicture(CAM_IMAGE_MODE_FHD, CAM_IMAGE_PIX_FMT_JPG);
-    
-    xbee_radio.write(0xcb); // indicate the start of an image data packet
-    
-    // Read the image data from the camera.
-    while (camera.getReceivedLength()) {
-      imageData = imageDataNext;
-      imageDataNext = camera.readByte();
-      
-      // If the image is not being read, then do not write to the buffer.
-      if (headFlag == 1) {
-        imageBuff[counter++] = imageDataNext;
+        xbee_radio.write(0xc8); // indicate start of normal data packet
+        xbee_radio.write(0x01); // nominal status
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(accelX); // send accelerometer reading
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(accelY); // send accelerometer reading
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(accelZ); // send accelerometer reading
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(gyroX); // send gyro reading
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(gyroY); // send gyro reading
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(gyroZ); // send gyro reading
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(temperature); // send temperature reading
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(batteryVoltage); // send voltage reading
+        xbee_radio.write(0xc9); // mark separation between values
+        xbee_radio.write(altitude); // send altitude reading
+        xbee_radio.write(0xca); // indicate end of normal data packet
+    } else if (flightMode == 2){
+        retentionServo.write(releasedAngle); // keeps servo in deployed position
 
-        if (counter >= BUFFER_SIZE) { // check to make sure there is still data to write
-          Serial.write(imageBuff, counter);
-          counter = 0;
+        delay(pictureDelay);
+
+        // Capture image in JPG format using an image resolution of one of the following: QQVGA, QVGA, VGA, HD, FHD.
+        camera.takePicture(CAM_IMAGE_MODE_QVGA, CAM_IMAGE_PIX_FMT_JPG);
+
+        xbee_radio.write(0xcb); // indicate the start of an image data packet
+        
+        // Read the image data from the camera.
+        while (camera.getReceivedLength()) {
+            imageData = imageDataNext;
+            imageDataNext = camera.readByte();
+            
+            // If the image is not being read, then do not write to the buffer.
+            if (headFlag == 1) {
+                imageBuff[counter++] = imageDataNext;
+
+                if (counter >= BUFFER_SIZE) { // check to make sure there is still data to write
+                xbee_radio.write(imageBuff, counter);
+                counter = 0;
+                }
+            }
+
+            // Notes the start of the image byte stream.
+            if (imageData == 0xff && imageDataNext == 0xd8) {
+                headFlag = 1;
+                imageBuff[counter++] = imageData;
+                imageBuff[counter++] = imageDataNext;  
+            }
+
+            // Notes the end of the image byte stream.
+            if (imageData == 0xff && imageDataNext ==0xd9) {
+                headFlag = 0;
+                // Write the image data to the serial monitor.
+                xbee_radio.write(imageBuff, counter);
+                counter = 0;
+                flightMode = 0;
+                break;
+            }
         }
-      }
-
-      // Notes the start of the image byte stream.
-      if (imageData == 0xff && imageDataNext == 0xd8) {
-        headFlag = 1;
-        imageBuff[counter++] = imageData;
-        imageBuff[counter++] = imageDataNext;  
-      }
-
-      // Notes the end of the image byte stream.
-      if (imageData == 0xff && imageDataNext ==0xd9) {
-          headFlag = 0;
-          // Write the image data to the serial monitor.
-          xbee_radio.write(imageBuff, counter);
-          counter = 0;
-          break;
-      }
     }
-  }
-  delay(50);
+    delay(1000);
 }
