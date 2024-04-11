04faff943534b65b6ccda44c58fd417f0573294f
Ryan Styron <ryanstyron1220@gmail.com>
2024-04-11 19:10:11 -0400

    Add labels for telemetry; other minor changes.

--- production/ground_station/ground_station.py
+++ production/ground_station/ground_station.py
@@ -1,4 +1,3 @@
-import time
 import numpy
 import serial
 import tkinter
@@ -21,14 +20,12 @@ flight_modes = {0: b'\x00',
 flight_mode = 5
 # Serial connection to the XBee Radio.
 xbee_radio : serial.Serial
-# Whether or not the flight is complete.
-flight_complete = False
 # Telemetry data.
-telemetry_data = {"altitude": [], "accel-x": 0.0,
-                    "accel-y": 0.0, "accel-z": 0.0,
-                    "gyro-x": 0.0, "gyro-y": 0.0,
-                    "gyro-z": 0.0, "temp": 0.0,
-                    "voltage": 0.0}
+telemetry_data = {"altitude": [], "accel-x": [],
+                    "accel-y": [], "accel-z": [],
+                    "gyro-x": [], "gyro-y": [],
+                    "gyro-z": [], "temperature": [],
+                    "voltage": []}
 
 def find_xbee_radio() -> None:
     global xbee_radio
@@ -36,6 +33,7 @@ def find_xbee_radio() -> None:
     ports = serial.tools.list_ports.comports()
     
     for port in ports:
+        # Check if the port is the XBee Radio (USB Serial Port for Windows Machine, USB UART for Mac).
         if 'USB Serial Port' in port.description or 'USB UART' in port.description:
             xbee_radio_port = port.device
             break
@@ -45,6 +43,18 @@ def find_xbee_radio() -> None:
     else:
         print("XBee Radio found.\n")
     xbee_radio = serial.Serial(xbee_radio_port, 9600)
+
+def set_flight_mode(mode: int) -> bool:
+    global flight_mode
+    
+    if mode == flight_mode:
+        return False
+    xbee_radio.write(flight_modes[mode])
+    flight_mode = mode
+    return True
+
+def retrieve_telemtry():
+    pass
 
 def retrieve_image() -> None:
     global xbee_radio
@@ -71,25 +81,12 @@ def retrieve_image() -> None:
         img_decode = cv.imdecode(image, cv.IMREAD_COLOR)
         # Display the image.
         cv.imshow("Rover Deployment Image", img_decode)
-        # Wait for a key press.
-        key = cv.waitKey(0)
-
-        # If the 's' key is pressed, save the image as a file in the resources folder.
-        if key == ord("s"):
-            cv.imwrite("production/resources/RoverDeploymentImage.jpeg", img_decode)
+        # Save the image.
+        cv.imwrite("production/resources/RoverDeploymentImage.jpeg", img_decode)
     except Exception as err:
         print(f'Other error occurred: {err}')
         print("\nExiting program.")
         exit()
-
-def set_flight_mode(mode: int) -> bool:
-    global flight_mode
-    
-    if mode == flight_mode:
-        return False
-    xbee_radio.write(flight_modes[mode])
-    flight_mode = mode
-    return True
 
 def run_ground_station() -> None:
     global xbee_radio
@@ -97,8 +94,24 @@ def run_ground_station() -> None:
     global telemetry_data
 
     def update_flight_mode_display():
-        lab_display_mode.config(text="Flight Mode " + str(flight_mode))
+        label_display_mode.config(text="Flight Mode " + str(flight_mode))
         root.after(1000, update_flight_mode_display)
+    def update_acceleration_display():
+        label_accel.config(text="Accel-X: " + str(telemetry_data["accel-x"]) \
+                            + ", Accel-Y: " + str(telemetry_data["accel-y"]) \
+                            + ", Accel-Z: " + str(telemetry_data["accel-z"]))
+        root.after(1000, update_acceleration_display)
+    def update_gyro_display():
+        label_gyro.config(text="Gyro-X: " + str(telemetry_data["gyro-x"]) \
+                            + ", Gyro-Y: " + str(telemetry_data["gyro-y"]) \
+                            + ", Gyro-Z: " + str(telemetry_data["gyro-z"]))
+        root.after(1000, update_gyro_display)
+    def update_temperature_display():
+        label_temperature.config(text="Temp (C): " + str(telemetry_data["temperature"]))
+        root.after(1000, update_temperature_display)
+    def update_voltage_display():
+        label_voltage.config(text="Voltage: " + str(telemetry_data["voltage"]))
+        root.after(1000, update_voltage_display)
     def set_mode_0():
         set_flight_mode(0)
     def set_mode_1():
@@ -110,23 +123,24 @@ def run_ground_station() -> None:
     def confirm_terminate():
         if messagebox.askyesno(title="Termination Confirmation", message="CONFIRM TERMINATION"):
             set_flight_mode(5)
+            store_telemetry_data()
+            print("Telemetry stored.\nExiting program.")
             exit()
-    def retrieve_telemetry():
+    def request_telemetry():
         if flight_mode != 1:
-            root.after(1000, retrieve_telemetry)
+            root.after(1000, request_telemetry)
             pass
-
-        root.after(1000, retrieve_telemetry)
+        # retrieve_telemetry()
+        root.after(1000, request_telemetry)
     root = tkinter.Tk()
     root.title("Ground Station")
     root.geometry("300x200")
     
     frame = Frame(root)
     frame.pack()
-    lab_display_mode = Label(frame, text="Flight Mode " + str(flight_mode), font=("Arial", 12, "bold"))
-    update_flight_mode_display()
-    
-    lab_display_mode.pack()
+
+    label_display_mode = Label(frame, text="Flight Mode " + str(flight_mode), font=("Arial", 12, "bold"))
+    label_display_mode.pack()
     label_select_mode = Label(frame, text="Select Flight Mode", font=("Arial", 10, "bold"), fg="red")
     label_select_mode.pack()
 
@@ -140,22 +154,57 @@ def run_ground_station() -> None:
     button_5.pack()
 
     #TODO: Implement telemetry data retrieval and plot the data.
-    retrieve_telemetry()
+    request_telemetry()
 
     # plot telemetry data
     fig = pyplot.Figure()
     ax = fig.add_subplot(111)
-    ax.set_ylim(0, 100)
+    ax.set_ylim(0, 1500)
     ax.set_xlim(0, 100)
     ax.set_title("Altitude vs Time")
-    ax.set_xlabel("Time (s)")
+    ax.set_xlabel("Time (sec)")
     ax.set_ylabel("Altitude (m)")
+    
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
     canvas.get_tk_widget().pack()
+
+    # to the right of the plot, display the non-altitude telemetry data
+    label_telemetry = Label(frame, text="Telemetry Data", font=("Arial", 10, "bold"), fg="red")
+    label_telemetry.pack()
+    label_accel = Label(frame, text="Accel-X: " + str(telemetry_data["accel-x"]) \
+                        + ", Accel-Y: " + str(telemetry_data["accel-y"]) \
+                        + ", Accel-Z: " + str(telemetry_data["accel-z"]))
+    label_accel.pack()
+    label_gyro = Label(frame, text="Gyro-X: " + str(telemetry_data["gyro-x"]) \
+                        + ", Gyro-Y: " + str(telemetry_data["gyro-y"]) \
+                        + ", Gyro-Z: " + str(telemetry_data["gyro-z"]))
+    label_gyro.pack()
+    label_temperature = Label(frame, text="Temp (C): " + str(telemetry_data["temperature"]))
+    label_temperature.pack()
+    label_voltage = Label(frame, text="Voltage: " + str(telemetry_data["voltage"]))
+    label_voltage.pack()
+
+    update_flight_mode_display()
+    update_acceleration_display()
+    update_gyro_display()
+    update_temperature_display()
+    update_voltage_display()
     
     root.mainloop()
+
+def store_telemetry_data() -> None:
+    global telemetry_data
+    file = open("production/resources/telemetry_data.txt", "w")
+
+    for key in telemetry_data:
+        file.write(key + ": " + str(telemetry_data[key]) + "\n")
+    file.close()
 
 if __name__ == "__main__":
     find_xbee_radio()
     run_ground_station()
+    # Will only be reached if the user ends the program without properly terminating.
+    store_telemetry_data()
+    print("Telemetry stored.\nExiting program.")
+    exit()
